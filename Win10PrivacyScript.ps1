cac9634e036993df2e584e6658fb457aa77c2fa2
mikejmcguirk <62523234+mikejmcguirk@users.noreply.github.com>
2020-03-22 20:00:37 -0400

    Add files via upload
    Windows version check logic expanded

--- Win10PrivacyScript.ps1
+++ Win10PrivacyScript.ps1
@@ -6,18 +6,58 @@ Function Write-Log {
     Write-Host $userInput
 }
 
-#Main Script Body
+#Check Windows Version
 
-$platformCheck = [System.Environment]::OSVersion.Platform -eq 'Win32NT'
-$versionCheck = (Get-ItemProperty -Path `
-    'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion' CurrentMajorVersionNumber).CurrentMajorVersionNumber `
-     -eq 10
-$architectureCheck = (Get-WmiObject Win32_OperatingSystem).OSArchitecture -eq '64-bit'
+$expectedPlatform = 'Win32NT'
+$expectedArchitecture = '64-bit'
+$expectedMajorVersion = 10
+$expectedMinorVersion = 0
+$expectedBuildNumber = 18363
+$expectedRevision = 720
 
-if ($platformCheck -and $versionCheck -and $architectureCheck) {
-    Write-Log 'Script running on Windows 10 64-bit. Proceeding'
-}
-else {
+$winVer = New-Object -TypeName PSObject
+
+$winVer | Add-Member -MemberType NoteProperty -Name Platform `
+    -Value $([System.Environment]::OSVersion.Platform)
+$winVer | Add-Member -MemberType NoteProperty -Name Architecture `
+    -Value $(Get-WmiObject Win32_OperatingSystem).OSArchitecture
+$winVer | Add-Member -MemberType NoteProperty -Name MajorVersion `
+    -Value $(Get-ItemProperty -Path `
+   'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion' CurrentMajorVersionNumber).CurrentMajorVersionNumber
+$winVer | Add-Member -MemberType NoteProperty -Name MinorVersion `
+    -Value $(Get-ItemProperty -Path `
+    'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion' CurrentMinorVersionNumber).CurrentMinorVersionNumber
+$winVer | Add-Member -MemberType NoteProperty -Name Build `
+    -Value $(Get-ItemProperty -Path `
+    'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion' CurrentBuildNumber).CurrentBuildNumber
+$winVer | Add-Member -MemberType NoteProperty -Name Revision `
+    -Value $(Get-ItemProperty -Path `
+    'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion' UBR).UBR
+
+if (($expectedPlatform -ne $winVer.Platform) -or `
+    ($expectedArchitecture -ne $winVer.Architecture) -or `
+    ($expectedMajorVersion) -ne $winVer.MajorVersion) {
     Write-Log 'Script not running on Windows 10 64-bit. Exiting'
+    exit
 }
 
+#$successfulCheck = 'Script running on an up-to-date Windows 10 64-bit system. Continuing...'
+
+if ($expectedMinorVersion -gt $winVer.MinorVersion) {
+    Write-Log ('Minor version must be at least ' + $expectedMinorVersion + '. Exiting')
+    exit
+}
+elseif ($expectedMinorVersion -eq $winVer.MinorVersion) {
+    if ($expectedBuildNumber -gt $winVer.Build) {
+        Write-Log ('Current build ' + $winVer.Build + ' is less than minimum build ' + $expectedBuildNumber + '. Exiting')
+        exit
+    }
+    elseif ($expectedBuildNumber -eq $winVer.Build) {
+        if ($expectedRevision -gt $winVer.Revision) {
+            Write-Log ('Current revision ' + $winVer.Revision + ' is less than expected revision ' + $expectedRevision + '. Exiting')
+            exit
+        }
+    }
+}
+
+Write-Log "Script running on an up-to-date Windows 10 64-bit system. Continuing..."
