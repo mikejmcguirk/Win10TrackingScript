0f8a190c5a24d0f639a03e0427ff92b08a0d357d
mikejmcguirk <62523234+mikejmcguirk@users.noreply.github.com>
2020-04-05 17:58:31 -0400

    Add files via upload
    Added changes that can only be run when logged in as Administrator
    
    Script now logs its output

--- Win10TrackingScript.ps1
+++ Win10TrackingScript.ps1
@@ -1,13 +1,32 @@
-# Script to Disable Windows Tracking Utilities
+# Script to Disable Windows Tracking
 # WIP
 # By Mike J. McGuirk
 
 #Create Functions
 
+$currentDir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+$logName = '\Win10TrackingLog_'
+$scriptStart = Get-Date -Format "yyyy-mm-dd_HHmmss"
+$outputFile = ($currentDir + $logName + $scriptStart + ".txt")
+
 Function Write-Log {
     Param ([string]$userInput)
 
     Write-Host $userInput
+    Add-Content -Path $outputFile -Value $userInput
+}
+
+Function If-Admin {
+    Param(
+        [parameter(position=0)]
+        $adminStatus, 
+        [parameter(position=1)]
+        [string]$userInput
+    )
+
+    if($adminStatus) {
+        Invoke-Expression $userInput
+    }
 }
 
 Write-Log ''
@@ -63,28 +82,75 @@ elseif ($expectedMinorVersion -eq $winVer.MinorVersion) {
     }
 }
 
-Write-Log "Script running on an up-to-date Windows 10 64-bit system. Continuing"
+Write-Log 'Script running on an up-to-date Windows 10 64-bit system. Continuing'
+Write-Log ''
+
+$isAdmin = $False
+
+if (([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent() `
+    ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Log "Running as administrator"
+    $isAdmin = $True
+} else {
+    Write-Log "Not running as administrator. Some fixes will be unable to run"
+}
+
 Write-Log ''
 
 #Implement Privacy Fixes
+
+If-Admin $isAdmin 'Write-Log ''Disallowing Cortana'''
+If-Admin $isAdmin 'Set-ItemProperty -Path `
+    ''Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\Windows Search'' -Name ''Allow Cortana'' -Value 0'
 
 Write-Log 'Removing and disabling Recent Items and Frequent Places'
 Remove-Item ($env:APPDATA + '\Microsoft\Windows\Recent\*.*') -Force
 Remove-Item ($env:APPDATA + '\Microsoft\Windows\Recent\AutomaticDestinations\*.*') -Force
 Remove-Item ($env:APPDATA + '\Microsoft\Windows\Recent\CustomDestinations\*.*') -Force
-Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Start_TrackDocs' -Value 0
+Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' `
+	-Name 'Start_TrackDocs' -Value 0
 
-Write-Log 'Disabling Activity Feed/Timeline and deleting old files'
+Write-Log 'Disabling Connected Device Platform Service'
+$globalSettings = ($env:LOCALAPPDATA + '\ConnectedDevicesPlatform\CDPGlobalSettings_Copy.cdp')
+
+Write-Log 'Disabling Activity Feed/Timeline'
+If-Admin $isAdmin 'Set-ItemProperty -Path ''Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System'' -Name ''EnableActivityFeed'' -Value 0'
+If-Admin $isAdmin 'Set-ItemProperty -Path ''Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System'' -Name ''PublishUserActivities'' -Value 0'
+If-Admin $isAdmin 'Set-ItemProperty -Path ''Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System'' -Name ''UploadUserActivities'' -Value 0'
+Write-Log 'Deleting old Activity Feed files. Feature only disabled when running as admin'
 Remove-Item ($env:LOCALAPPDATA + '\Microsoft\Windows\History\*.*') -Force -Recurse
 
-Write-Log 'Disabling "Improve Typing" feature'
+Write-Log 'Disabling "Improve Typing" Feature'
 Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Software\Microsoft\Input\TIPC' -Name 'Enabled' -Value 0
+If-Admin $isAdmin 'Set-ItemProperty -Path ''Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Input\TIPC'' -Name ''Enabled'' -Value 0'
 
 Write-Log 'Disabling Locally Relevant Content'
 Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Control Panel\International\User Profile' -Name 'HttpAcceptLanguageOptOut' -Value 1
 
 Write-Log 'Disabling Advertising Info Sends'
 Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo' -Name 'Enabled' -Value 0
+
+If-Admin $isAdmin 'Write-Log ''Disabling Superfetch'''
+$superCheck = Get-Service -Name Superfetch -ErrorAction SilentlyContinue
+if ($superCheck.Length -gt 0) {
+    Stop-Service -Name 'Superfetch'
+    Disable-Service -Name 'Superfetch' 
+}
+If-Admin $isAdmin 'Set-ItemProperty -Path ''Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters'' `
+    -Name ''EnableSuperfetch'' -Value 0'
+
+If-Admin $isAdmin 'Write-Log ''Stopping and disabling Device Management Wireless Application Protocol (WAP) Push message Routing Service'''
+If-Admin $isAdmin 'Stop-Service -Name ''dmwappushservice'''
+If-Admin $isAdmin 'Set-Service -Name ''dmwappushservice'' -StartupType Disabled'
+
+If-Admin $isAdmin 'Write-Log ''Stopping and disabling Connected Devices Platform Service'''
+If-Admin $isAdmin 'Stop-Service -Name ''CDPSvc'''
+If-Admin $isAdmin 'Set-Service -Name ''CDPSvc'' -StartupType Disabled'
+
+If-Admin $isAdmin 'Write-Log ''Stopping and disabling Microsoft Compabitibility Appraiser task'''
+If-Admin $isAdmin 'Stop-ScheduledTask -TaskName ''\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser'''
+If-Admin $isAdmin 'Disable-ScheduledTask -TaskName ''\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser'' | Out-Null'
 
 Write-Log ''
 Write-Log 'Tracking script complete. Restart recommended for all changes to take effect'
